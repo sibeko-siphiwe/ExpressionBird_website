41dd511e7029ef1ea330453fb0efdfde6376b221
Sibeko Siphiwe <2021934100@ufs4life.ac.za>
2025-12-17 16:12:16 +0200

    Added Animation

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ window.onscroll = function() {
         navabr.classList.remove('scrolled');
     }
 };
+
+//  Animation
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target)
+            }
+            else{
+                entry.target.classList.remove('show');
+            }
+        });
+    },
+    {threshold: 0.15}
+);
+
+document.querySelectorAll('.animate')
+    .forEach(el => observer.observe(el));
